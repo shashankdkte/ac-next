999707030a130475ada7a02aae7f2220811570a7
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-02 00:02:00 +0530

    Navigate programatically

--- 02-file-based-routing/pages/clients/[id]/index.js
+++ 02-file-based-routing/pages/clients/[id]/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from "next/router";
 function ClientPage()
 {
+  const router = useRouter();
+  console.log(router.query);
+  function loadProjectHandler() {
+    router.replace("/clients/max/projecta");
+    // router.push({
+    //   pathname: "/clients/[id]/[clientprojectid]",
+    //   query: {
+    //     id:"max" , clientprojectid:"projectaid"
+    //   }
+    // })
+}
+
   return <div>
     <h2>ClientPage</h2>
-   
+   <button onClick={loadProjectHandler}>Load Project</button>
   </div>
 }
 
