f1aa26423990db9d9568453e9566b22100927db5
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-01 21:52:07 +0530

    Building Nested Dynamic Routes

---
+++ 02-file-based-routing/pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,8 @@
+function ClientProjectPage()
+{
+  return <div>
+    <h2>ClientProjectPage</h2>
+  </div>
+}
+
+export default ClientProjectPage; 
---
+++ 02-file-based-routing/pages/clients/[id]/index.js
@@ -0,0 +1,8 @@
+function ClientPage()
+{
+  return <div>
+    <h2>ClientPage</h2>
+  </div>
+}
+
+export default ClientPage; 
