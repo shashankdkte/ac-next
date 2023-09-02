57a5fda777cfaa0dd64abdbebcbbdf9d74912f1f
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-02 17:07:55 +0530

    Running Server side code and using the FileSystem

--- 04-pre-render-data-fetch/pages/index.js
+++ 04-pre-render-data-fetch/pages/index.js
@@ -1,11 +1,29 @@
-function HomePage() {
+import path from "path";
+import fs from "fs/promises";
+function HomePage(props) {
+
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product1</li>
-      <li>Product2</li>
-      <li>Product3</li>
+      
+      {products && products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   )
 }
 
+export async function getStaticProps() {
+  
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products
+    }
+  }
+}
 export default HomePage;
