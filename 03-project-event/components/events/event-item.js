4d21244ee7d3e162b0b2f09c5f39faa7f22dbeb2
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-03 15:46:21 +0530

    Adding Image optimized

--- 03-project-event/components/events/event-item.js
+++ 03-project-event/components/events/event-item.js
@@ -5,6 +5,7 @@ import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
+import Image from 'next/image';
 const EventItem = (props) => {
   const { title, image, date, address, id } = props;
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -16,7 +17,7 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
   return (
     <li className={classes.item}> 
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
--- 03-project-event/pages/index.js
+++ 03-project-event/pages/index.js
@@ -1,10 +1,14 @@
 import { getFeaturedEvents } from "../helpers/api.util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 function HomePage(props) {
 
 
   return <div>
-  
+    <Head>
+      <title>NextJS Events</title>
+      <meta name="description" content="Find a lot of great events that allow you to survive" />
+  </Head>
     <EventList  items = {props.events}/>
   </div>
 }
