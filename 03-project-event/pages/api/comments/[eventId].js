b1e44c760e7caf4b94d6ca014d377870ad80af56
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-03 20:05:37 +0530

    Connection MongoDb

--- 03-project-event/pages/api/comments/[eventId].js
+++ 03-project-event/pages/api/comments/[eventId].js
@@ -1,8 +1,12 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   
+  const client = await MongoClient.connect("mongodb+srv://shashankdkte:shashankdkte@ac-next.raedmop.mongodb.net/events?retryWrites=true&w=majority");
   const eventId = req.query.eventId;
   if (req.method === "POST")
   {
+   
     const { email, name, text } = req.body;
     if (!email.includes("@") || !name || name.trim() === "" || 
       !text || text.trim() === "")
@@ -16,12 +20,16 @@ function handler(req, res) {
     console.log(email, name, text);
 
     const newComment = {
-      id: new Date().toISOString(),
+      eventId,
       email,
       name,
       text
     }
 
+     const db = client.db();
+    const result = await db.collection('comments').insertOne(newComment);
+   
+    newComment.id = result.insertedId;
     res.status(201).json({
       message: "Added comment",
       comment:newComment
@@ -29,14 +37,12 @@ function handler(req, res) {
   }
   else if (req.method === "GET")
   {
-    
-    const dummyLists = [
-      {id:"C1",name:"Max",text:"A firstComment"},
-      {id:"C2",name:"Manuel",text:"A Second Comment"}
-    ]
-
-    res.status(200).json({comments:dummyLists})
+      
+    const db = client.db();
+    const documents = await db.collection("comments").find().sort({ _id: -1 }).toArray();
+    res.status(200).json({comments:documents})
   }
+  client.close();
 }
 
 export default handler;
--- 03-project-event/pages/api/newsletter.js
+++ 03-project-event/pages/api/newsletter.js
@@ -12,9 +12,9 @@ async function handler(req, res) {
       })
       return;
     }
-    const client = await MongoClient.connect("mongodb+srv://shashankdkte:shashankdkte@ac-next.raedmop.mongodb.net/newsletter?retryWrites=true&w=majority");
+    const client = await MongoClient.connect("mongodb+srv://shashankdkte:shashankdkte@ac-next.raedmop.mongodb.net/events?retryWrites=true&w=majority");
     const db = client.db();
-    await db.collection('emails').insertOne({ email: userEmail });
+    await db.collection('newsletter').insertOne({ email: userEmail });
     client.close();
     res.status(201).json({
       message:"Signed Up"
