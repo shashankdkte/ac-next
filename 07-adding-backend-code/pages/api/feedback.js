254db782820556c1153117dcf790fe6cf926b5d4
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-03 16:33:14 +0530

    Sending Request to API Routes

--- 07-adding-backend-code/pages/api/feedback.js
+++ 07-adding-backend-code/pages/api/feedback.js
@@ -1,7 +1,35 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({
-    message:"This works"
-  })
+
+  if (req.method === "POST")
+  {
+    const email = req.body.email;
+    const feedback = req.body.feedback;
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      feedback: feedback
+    };
+
+    //store in a database or a file
+    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({
+      message: "Success",
+      feedback: newFeedback
+    })
+  }
+  else
+  {
+    res.status(200).json({
+      message: "This works"
+    })
+  }
 };
 
 export default handler;
